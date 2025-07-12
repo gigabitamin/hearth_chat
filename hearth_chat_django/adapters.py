746d5e72965bfdb1d49c2f3e21d37c8e8d8eca63
gigabitamin <gigabitamin@gmail.com>
2025-07-13 00:42:25 +0900

    Auto commit at $(date '+m-H:S')

--- hearth_chat_django/adapters.py
+++ hearth_chat_django/adapters.py
@@ -2,6 +2,8 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def get_login_redirect_url(self, request):
+        print("=== get_login_redirect_url 호출됨 ===")
         return '/accounts/popup-close/'
     def get_connect_redirect_url(self, request, socialaccount):
+        print("=== get_connect_redirect_url 호출됨 ===")
         return '/accounts/popup-close/' 
