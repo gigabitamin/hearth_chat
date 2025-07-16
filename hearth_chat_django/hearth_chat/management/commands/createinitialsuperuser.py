a105363d0cab493eb134fd6e167e60d98d28a781
gigabitamin <gigabitamin@gmail.com>
2025-07-17 00:00:28 +0900

    Auto commit at $(date '+m-H:S')

--- hearth_chat_django/hearth_chat/management/commands/createinitialsuperuser.py
+++ hearth_chat_django/hearth_chat/management/commands/createinitialsuperuser.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
         User = get_user_model()
         username = 'gigabitamin'
         email = 'gigabitamin@gmail.com'
-        password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+        password = 'windmill4u@'
         user, created = User.objects.get_or_create(username=username, defaults={'email': email})
         user.email = email
         user.is_superuser = True
