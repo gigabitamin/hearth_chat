d08d5508e0cca60a9c554d60928ec88a865bab6a
gigabitamin <gigabitamin@gmail.com>
2025-08-10 05:44:39 +0900

    Auto commit at 10-2025-08 5:44:38

--- hearth_chat_django/urls.py
+++ hearth_chat_django/urls.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
     path('accounts/popup-close/', TemplateView.as_view(template_name='social_login_redirect.html'), name='popup-close'),
