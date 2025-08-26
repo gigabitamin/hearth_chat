342aace40e0172e0a5af131f09f64bc331532527
gigabitamin <gigabitamin@gmail.com>
2025-08-27 02:46:16 +0900

    Auto commit at 27-2025-08 2:46:16

--- hearth_chat_react/android/app/src/main/java/com/gbrabbit/hearthchat/MainActivity.java
+++ hearth_chat_react/android/app/src/main/java/com/gbrabbit/hearthchat/MainActivity.java
@@ -1,5 +1,23 @@
 package com.gbrabbit.hearthchat;
 
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+            cookieManager.setAcceptThirdPartyCookies(getBridge().getWebView(), true);
+            WebSettings settings = getBridge().getWebView().getSettings();
+            settings.setDomStorageEnabled(true);
+            settings.setJavaScriptEnabled(true);
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        } catch (Exception ignored) {
+        }
+    }
+}
